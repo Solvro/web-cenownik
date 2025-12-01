cfe183a8e4e142b8ad974cce71e5a5f8cda8c5ad
Grzegorz Diaconescu <grzes.business@gmail.com>
2025-12-01 16:32:37 +0100

    feat: charts

---
+++ src/app/api/info/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const users = [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+  ];
+  return NextResponse.json(users);
+}
